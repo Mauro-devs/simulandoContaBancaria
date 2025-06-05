43146f5f931e7d345e65cd3527b3b14b2d9f35f4
Mauro Barros <maurodbds@gmail.com>
2025-06-05 14:52:47 -0300

    projeto concluido

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ContaTerminal {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("oi");
-        
+        int numeroAgencia;
+        String agencia, nomeUsuario;
+        float saldo;
+
+        System.out.println("Informe o número da agência:");
+        numeroAgencia = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Informe o nome da agência:");
+        agencia = sc.nextLine();
+        System.out.println("Informe o nome do cliente:");
+        nomeUsuario = sc.nextLine();
+        System.out.println("Informe o saldo:");
+        saldo = sc.nextFloat();
+
+        System.out.println("Olá " + nomeUsuario+ ", obrigado por criar uma conta em nosso banco, sua agência é " +agencia + ", conta " + numeroAgencia+ " e seu saldo "+ saldo+ " já está disponível para saque");
     }
 }
